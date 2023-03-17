ff5fa0b2ef60ea1412423004c4232cc4dba644e2
Wycliffe <wycliffekiprono4@gmail.com>
2023-03-17 11:29:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob"; //declaring a global variable
+function upperCaseCustomerName() {  // function to uppercase the global variable
+    customerName = customerName.toUpperCase();  
+}
+
+function setBestCustomer() { // function to declare a global variable and assign it a value
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "bob";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "poor bob";
+}
+
